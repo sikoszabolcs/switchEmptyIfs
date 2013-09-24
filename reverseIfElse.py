d2545373cefd333142f60d222db2ff072ae38320
Szabolcs Siko <siko.szabolcs@gmail.com>
2013-09-24 15:37:24 +0300

    added script skeleton

---
+++ reverseIfElse.py
@@ -0,0 +1,23 @@
+def getFileContents(string fileName):
+	f = open(filename, 'r')
+	string fileContents = f.read()
+	f.close()
+	return fileContents
+
+def findIfWithEmptyBodyInString(string searchStr):
+
+
+def getElseBodyPart():
+
+def getIfExpression():
+
+def negateExpression():
+
+def switchIfBodyWithElseBody():
+
+
+def readAllFilesFromFolder(string folder):
+
+def main():
+	string fileContents = getFileContents("d:\Views\evoSiSz0_ESF_CoreDev_002.001_AT_pre\ESF_CORE\dev\logic\src\BaseSI.cpp")
+	print fileContents
