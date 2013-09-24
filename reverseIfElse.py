f8a15ceb854dedf6def164187bc8ecdf37863fe1
Szabolcs Siko <siko.szabolcs@gmail.com>
2013-09-24 16:40:40 +0300

    added regular expression match of if else clause

--- reverseIfElse.py
+++ reverseIfElse.py
@@ -1,23 +1,31 @@
-def getFileContents(string fileName):
-	f = open(filename, 'r')
-	string fileContents = f.read()
+import re
+
+def getFileContents(fileName):
+	f = open(fileName, "r")
+	fileContents = f.read()
 	f.close()
 	return fileContents
 
-def findIfWithEmptyBodyInString(string searchStr):
+# group 0 - all matched
+# group 1 - expression inside if
+# group 2 - everything inside the else between the braces {...}
+def findIfWithEmptyBodyInString(searchStr):
+	for m in re.finditer(r"if\s*\((.+\))\s*\{\s*\}\s*else\s*\{((\{.*\}|[^{$}])*)\}", searchStr):
+		print 'IF Expression: %s \n +++++++ \n ELSE Contents: \n %s \n --------' % (m.group(1), m.group(2))
 
+#def getElseBodyPart():
 
-def getElseBodyPart():
+#def getIfExpression():
 
-def getIfExpression():
+#def negateExpression():
 
-def negateExpression():
+#def switchIfBodyWithElseBody():
 
-def switchIfBodyWithElseBody():
-
-
-def readAllFilesFromFolder(string folder):
+#def readAllFilesFromFolder(folder):
 
 def main():
-	string fileContents = getFileContents("d:\Views\evoSiSz0_ESF_CoreDev_002.001_AT_pre\ESF_CORE\dev\logic\src\BaseSI.cpp")
-	print fileContents
+	fileContents = getFileContents("d:\Views\evoSiSz0_ESF_CoreDev_002.001_AT_pre\ESF_CORE\dev\logic\src\BaseSI.cpp")
+	findIfWithEmptyBodyInString(fileContents)
+
+if __name__ == "__main__":
+	main()
