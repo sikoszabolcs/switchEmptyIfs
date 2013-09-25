767b69f44c62709537bb4dab7ad3d9bfcec5389e
Szabolcs Siko <siko.szabolcs@gmail.com>
2013-09-25 16:28:59 +0300

    turns out python cannot do nested baracket matching

--- reverseIfElse.py
+++ reverseIfElse.py
@@ -10,8 +10,25 @@ def getFileContents(fileName):
 # group 1 - expression inside if
 # group 2 - everything inside the else between the braces {...}
 def findIfWithEmptyBodyInString(searchStr):
-	return re.finditer(r"if\s*\((.+\))\s*\{\s*\}\s*else\s*\{((\{.*\}|[^{$}])*)\}", searchStr)
+	#if\s*\(+.*\)+\s*\{\s*\}\s*else\s*\{((?:[^{}]+?|\{(?1)\}))+?\}
+	#<[^<>]*(((?<Open><)[^<>]*)+((?<Close-Open>>)[^<>]*)+)*(?(Open)(?!))> 
+	#\{(?:[^|}]+)?([^}]+)\}
+	#\[\[(?:[^\]|]*\|)?([^\]|]*)\]\]
+	#if\s*\(+.*\)+\s*\{\s*\}\s*else\s*\{((?:[^{}]++|\{(?1)\}))++\}
+	#\{((?:[^{}]++|\{(?1)\}))++\}
+	#Clear form: \{((?:[^{}]++|\{(?1)\}))++\}
+	#return re.finditer(r"if\s*\(+.*\)+\s*\{{1}\s*\}{1}\s*else\s*\{((?:[^{}]++|\{(?1)\}))++\}", searchStr)
+	#\{([^{}]+|\{(\1)\})+
+	#if\s*\(+\s*.*\s*\)+\s*\{\s*\}\s*else\s*\{([^{}]+|\{.*\})+
+
+	#\{$(?:\n^.+$)*\n^\}$
+	#prog = re.compile('if\s*\(+.*\)+\s*\{{1}\s*\}{1}\s*else\s*\{$(?:\n^.+$)*\n^\}$', re.MULTILINE)
+	# This works: prog = re.compile('\{$.*?^\}$', re.DOTALL | re.MULTILINE)
+	#prog = re.compile('\{$.*?^\}$', re.DOTALL | re.MULTILINE)
+	#$\s*\{\s*\}
+	prog = re.compile(r'\{$.*?^\}$', re.DOTALL | re.MULTILINE)
 	
+	return prog.finditer(searchStr)
 
 
 #def getElseBodyPart():
@@ -28,11 +45,16 @@ def main():
 	fileName = "d:\Views\evoSiSz0_ESF_CoreDev_002.001_AT_pre\ESF_CORE\dev\logic\src\BaseSI.cpp"
 	fileContents = getFileContents(fileName)
 	matches = findIfWithEmptyBodyInString(fileContents)
+	count = 0
 	for m in matches:
-		print 'IF Expression: %s \n +++++++ \n ELSE Contents: \n %s \n --------' % (m.group(1), m.group(2))
-		print 'start: %d end: %d' % (m.start(1), m.end(1))
+		print '--------------------------------------------\n'
+		print m.group()
+		print count
+		count += 1
+		print '++++++++++++++++++++++++++++++++++++++++++++\n'
 
-
+		#print 'IF Expression: %s \n +++++++ \n ELSE Contents: \n %s \n --------' % (m.group(1), m.group(2))
+		#print 'start: %d end: %d' % (m.start(1), m.end(1))
 
 if __name__ == "__main__":
 	main()
