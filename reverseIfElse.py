0f99b3675cbf961eea906f5357a7eb5416512b64
Szabolcs Siko <siko.szabolcs@gmail.com>
2013-10-02 15:02:10 +0300

    simple finds with embedded ifs working

--- reverseIfElse.py
+++ reverseIfElse.py
@@ -23,6 +23,103 @@ def findNestedString(searchStr):
 
 	return expressions
 
+def findMinimal(searchStr):
+
+	prog = re.compile(r'\{\s*\}\s*else\s*\{', re.DOTALL)
+	matches = prog.finditer(searchStr)
+	
+	posInSearchStr = 0
+
+	ifExpressions = []
+	elseBlocks = []
+	matchObjects = []
+
+	for m in matches:
+
+		matchObjects.append(m)
+
+		# find else block
+		openCurlyBraceCount = 1
+		posInSearchStr = m.end()
+
+		elseBlock = ""
+		while openCurlyBraceCount != 0:
+			curChar = searchStr[posInSearchStr]
+			if curChar == '{':
+				openCurlyBraceCount += 1
+			if curChar == '}':
+				openCurlyBraceCount -= 1
+
+			if openCurlyBraceCount == 0:
+				break
+
+			elseBlock += curChar
+			posInSearchStr += 1
+
+		elseBlocks.append(elseBlock)
+
+
+		# find if expression
+		openBraketCount = 1
+		posInSearchStr = m.start()
+
+		while curChar != ')': # !!!WEAKNESS: Error when searchStr contains invalid expression!
+			curChar = searchStr[posInSearchStr]
+			posInSearchStr -= 1
+
+		ifExpression = ""
+		while openBraketCount != 0:
+			curChar = searchStr[posInSearchStr]
+			
+			if curChar == ')':
+				openBraketCount += 1
+			if curChar == '(':
+				openBraketCount -= 1
+
+			if openBraketCount == 0:
+				break
+
+			ifExpression += curChar
+			posInSearchStr -= 1
+
+		ifExpressions.append(ifExpression[::-1])
+
+	return matchObjects, ifExpressions, elseBlocks
+
+def insertRevStart(author, insertPos):
+	revStart = "// @revstart[-] 20130926 " + author + " replace else query by a negated if query"
+
+
+def insertRevEnd(author, insertPos):
+	revEnd = "// @revend 20130926 " + author
+
+def determineIndentationOfRevStartCommentFromPos(ifExprStartPos, searchStr):
+	posInSearchStr = ifExprStartPos
+	indentation = ""
+	curChar = ''
+	while curChar != '\n' and posInSearchStr >= 0:
+		curChar = searchStr[posInSearchStr]
+		indentation += curChar
+		posInSearchStr -= 1
+
+	indentation = indentation[::-1] # reverse
+	try:
+		indentation = indentation[:indentation.index('if')]
+	except Exception, e:
+		raise Exception("Invalid IF expression!")
+
+	return indentation
+
+def insertElseIntoIf(searchStr):
+	matches, ifs, elses = findMinimal(searchStr)
+
+	result = searchStr
+
+	for m in reversed(matches):
+		result = result[:m.start()+1] + result[m.end():]
+
+	return result
+
 #def getElseBodyPart():
 
 #def getIfExpression():
@@ -34,9 +131,10 @@ def findNestedString(searchStr):
 #def readAllFilesFromFolder(folder):
 
 def main():
-	fileName = "d:\Views\evoSiSz0_ESF_CoreDev_002.001_AT_pre\ESF_CORE\dev\logic\src\BaseSI.cpp"
+	#fileName = "d:\Views\evoSiSz0_ESF_CoreDev_002.001_AT_pre\ESF_CORE\dev\logic\src\BaseSI.cpp"
+	fileName = "test.txt"
 	expressions = findNestedString(getFileContents(fileName))
 	print expressions
-	
+
 if __name__ == "__main__":
 	main()
--- test_reverseIfElse.py
+++ test_reverseIfElse.py
@@ -3,39 +3,59 @@ import unittest
 
 class TestReverseIfElse(unittest.TestCase):
 
-	def test_oneIf_ShouldResult_InOneResult(self):
-		testString = "if(a < b){}"
-		result = reverseIfElse.findNestedString(testString)
-		self.assertEqual(len(result), 1)
+	 def test_findMinimal(self):
+		testString = "if(a < b){}else{ alma korte banan }"
+	 	matches, ifs, elses = reverseIfElse.findMinimal(testString)
+	 	self.assertEqual(ifs[0], "a < b")
+	 	self.assertEqual(elses[0], " alma korte banan ")
+	 	for m in matches:
+	 		self.assertEqual(9, m.start())
+	 		self.assertEqual(16, m.end())
 
-	def test_oneIf_ShouldResult_InExpressionBetweenBrackets(self):
-		testString = "if(a < b){}"
-		result = reverseIfElse.findNestedString(testString)
-		self.assertEqual(result[0], "a < b")
+	 def test_findMinimal_with_complex_if_expression(self):
+		testString = "if(a \n< \nb){}else{ alma korte banan }"
+	 	matches, ifs, elses = reverseIfElse.findMinimal(testString)
+	 	self.assertEqual(ifs[0], "a \n< \nb")
+	 	self.assertEqual(elses[0], " alma korte banan ")
+	 	for m in matches:
+	 		self.assertEqual(11, m.start())
+	 		self.assertEqual(18, m.end())
 
-	def test_oneIfWithNestedBrackets_ShouldResult_InExpressionBetweenFirstPairOfBrackets(self):
-		testString = "if( (a == b) < (b + foo)){}"
-		result = reverseIfElse.findNestedString(testString)
-		self.assertEqual(result[0], " (a == b) < (b + foo)")
+	 def test_determineIndentationOfRevStartCommentFromPos(self):
+	 	testString = "\n\t  if(a < b){}else{ alma korte banan }"
+	 	result = reverseIfElse.determineIndentationOfRevStartCommentFromPos(14, testString)
+	 	self.assertEqual('\n\t  ', result)
 
-	def test_oneInvalidIf_ShoudlResult_InError(self):
-	 	testString = "if(a < b{}"
-	 	self.assertRaises(NameError, reverseIfElse.findNestedString, testString)
+	 def test_noEnterBeforeIf(self):
+		testString = "if(a < b){}else{ alma korte banan }"
+	 	result = reverseIfElse.determineIndentationOfRevStartCommentFromPos(9, testString)
+	 	self.assertEqual('', result)
 
-	def test_twoIf_ShouldResult_InTwoResults(self):
-	 	testString = "if(a < b) {} if(c == d) {}"
-	 	result = reverseIfElse.findNestedString(testString)
-	 	self.assertEqual(len(result), 2)
+	 def test_noIfBeforeElse(self):
+	 	testString = "f(a < b){}else{ alma korte banan }"
+	 	self.assertRaises(Exception, reverseIfElse.determineIndentationOfRevStartCommentFromPos, 0, testString)
 
-	def test_missingCurlyBraces_ShouldResult_InNoMatch(self):
-		testString = "if(a < b)"
-	 	result = reverseIfElse.findNestedString(testString)
-	 	self.assertEqual(len(result), 0)
+	 def test_withFile(self):
+	 	testString = "if(a>b){}else{if(alma){}else{int t =0;\nint c=1;}}"
+		matches, ifs, elses = reverseIfElse.findMinimal(testString)
+		self.assertEqual(2, len(ifs))
+		self.assertEqual(2, len(elses))
 
-	def test_anythingOtherThanWhitespaseWithinCurlyBraces_ShouldResult_InNoMatch(self):
-		testString = "if(a < b){ something }"
-	 	result = reverseIfElse.findNestedString(testString)
-	 	self.assertEqual(len(result), 0)
+	 def test_insertElseIntoIf(self):
+		testString = "if(a>b){}else{test}"
+		result = reverseIfElse.insertElseIntoIf(testString)
+		self.assertEqual("if(a>b){test}", result)
+
+	 def test_insertElseIntoIf_Variation(self):
+	 	testString = "if(a>b){}else{\n\ttest();\n\tint i=0;\n}"
+		result = reverseIfElse.insertElseIntoIf(testString)
+		self.assertEqual("if(a>b){\n\ttest();\n\tint i=0;\n}", result)
+	 
+	 def test_insertElseIntoIf_with_embedded_If(self):
+	 	testString = "if(a>b){}else{if(elephant>moon){}else{everythingFine();}}"
+		result = reverseIfElse.insertElseIntoIf(testString)
+		self.assertEqual("if(a>b){if(elephant>moon){everythingFine();}}", result)
+
 
 if __name__ == '__main__':
 	unittest.main()
